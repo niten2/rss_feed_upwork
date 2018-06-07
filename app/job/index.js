25204aad16781577a3489a7930f7b5aee8fb9a1c
Niten <niten2@yandex.ru>
2018-06-07 21:22:44 +0700

    fix job

--- app/job/index.js
+++ app/job/index.js
@@ -1,18 +1,21 @@
 import schedule from "node-schedule"
+import logger from "app/services/logger"
 import { getRss, checkAndCreateFeeds } from "app/services/rss"
 
-// NOTE rule - every minute
-let rule = new schedule.RecurrenceRule()
-rule.minute = new schedule.Range(0, 59, 1)
-schedule.scheduleJob(rule, async () => { await run() })
+const getRuleEveryMinute = () => {
+  let rule = new schedule.RecurrenceRule()
+  rule.minute = new schedule.Range(0, 59, 1)
+  return rule
+}
 
-const run = async () => {
+const main = async () => {
   try {
-    const rss = await getRss()
-    await checkAndCreateFeeds(rss)
+    await checkAndCreateFeeds(await getRss())
 
     logger.info("finish checkAndCreateFeeds")
   } catch (err) {
     logger.error(err)
   }
 }
+
+schedule.scheduleJob(getRuleEveryMinute(), async () => { await main() })
