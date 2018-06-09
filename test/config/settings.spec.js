499319e0c3db407a732eeb105c7a9f90c0a6c24e
Niten <niten2@yandex.ru>
2018-06-09 10:49:52 +0700

    fix tests

--- test/config/settings.spec.js
+++ test/config/settings.spec.js
@@ -1,15 +1,16 @@
-import { expect } from 'chai'
 import settings from "config/settings"
 
-xdescribe('settings', () => {
-  it("correct vars", async () => {
-    expect(settings.name).to.have.eq("api_gateway")
-    expect(settings.env).to.have.eq(process.env.NODE_ENV)
-    expect(settings.port).to.have.eq(process.env.PORT)
-    expect(settings.storage).to.have.eq("./db/database.test.sqlite")
-    expect(settings.jwt_secret_key).to.have.eq(process.env.JWT_SECRET_KEY)
-    expect(settings.crmUri).to.have.eq(process.env.CRM_URI)
-    expect(settings.vkUri).to.have.eq(process.env.VK_URI)
-    expect(settings.instaUri).to.have.eq(process.env.INSTA_URI)
+describe('settings', () => {
+  it("should have valid env", async () => {
+    expect(settings.name).toEqual("rss_upwork")
+    expect(settings.env).toEqual(process.env.NODE_ENV)
+
+    expect(settings.databaseUrl).toEqual(process.env.DATABASE_URL)
+
+    expect(settings.rss_upwork).toEqual(process.env.RSS_UPWORK)
+    expect(settings.email_to).toEqual(process.env.EMAIL_TO)
+
+    expect(settings.mailgun.api_key).toEqual(process.env.MAILGUN_API_KEY)
+    expect(settings.mailgun.domain).toEqual(process.env.MAILGUN_DOMAIN)
   })
 })
