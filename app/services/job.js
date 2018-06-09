10a4d176d7fa631794069458f48029b2c7b32d77
Niten <niten2@yandex.ru>
2018-06-09 10:41:10 +0700

    edit job

--- app/services/job.js
+++ app/services/job.js
@@ -1,7 +1,7 @@
+import logger from "app/services/logger"
 import { Feed } from "app/models"
 import { sendEmailMailgun } from "app/services/email"
 import { getRss } from "app/services/rss"
-import logger from "app/services/logger"
 
 export const runJob = async () => {
   const rss = await getRss()
