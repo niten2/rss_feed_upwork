e7606d06c1a68ba73d008b7518e5e3825eb0ab75
Niten <niten2@yandex.ru>
2018-06-11 14:54:39 +0700

    remove fake server

--- app/job/index.js
+++ app/job/index.js
@@ -8,16 +8,3 @@ const getRuleEveryMinute = () => {
 }
 
 schedule.scheduleJob(getRuleEveryMinute(), runJobWithErrorLog)
-
-
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-  var result = 'App is running'
-  response.send(result);
-}).listen(app.get('port'), function() {
-  console.log('App is running, server is listening on port ', app.get('port'))
-});
